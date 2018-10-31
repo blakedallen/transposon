c192beab3d2fa1e60533ec3b06b5fdc2579a95aa
blakedallen <blakedallen@gmail.com>
2018-10-30 18:22:10 -0700

    initial build of the base class and project. TODO: add unit tests to confirm that the vectors are being created correctly (no off by one errors) next, write up comprehensive docs, next: come up with a few more examples to test the vector representation of genetic evolution. Also possible areas to explore are different mutation functions and such

---
+++ base.py
@@ -0,0 +1,138 @@
+import numpy as np
+from random import randint
+
+class Transposon(object):
+	""" Transposon is our general class used for evolutionary 
+	algorithms applied to vectors"""
+
+	def __init__(self, 
+			vector_len=100, 
+			fitness_func=None,
+			min_value=0,
+			max_value=100,
+			population_size=1000,
+			max_fitness=1.0,
+			max_generations=1000,
+			mutation_rate=0.13,
+			winner_pool=0.10,
+			verbose=False):
+		
+		self.vector_len = vector_len
+		self.fitness_func = fitness_func
+		self.population_size = population_size
+		self.max_fitness = max_fitness
+		self.verbose = verbose
+		self.winner_pool = winner_pool
+		self.values = [x for x in range(min_value, max_value+1)]
+		self.max_generations = max_generations
+		self.mutation_rate = mutation_rate
+		self.min_value = min_value
+		self.max_value = max_value
+		self.initialize()
+
+	def initialize(self):
+		""" sets up our population and asserts 
+		our fitness func is of correct type
+		"""
+		#setup a random vector and assert that our fitness_func is of correct type
+		random_vector = self.create_vector()
+		#use our fitness function, assert that the value is correct
+		fitness = self.fitness_func(random_vector)
+		assert fitness >= 0.0
+		assert fitness <= self.max_fitness
+		#now create our population
+		population = [random_vector]
+		for i in range(1,self.population_size):
+			population.append(self.create_vector())
+		self.population = population
+
+
+	def create_vector(self, replace=False):
+		"""
+		Create a random vector
+		replace = whether or not we can replace values (default false, ie: each value is unique)
+		"""
+		return np.random.choice(self.values, self.vector_len, replace=replace).tolist()
+
+	def mutate(self):
+		""" create mutations randomly based on the mutation rate
+		preserves winner pool so that the best individuals aren't mutated
+		"""
+		mutated_population = []
+		
+		for i,individual in enumerate(self.population):
+			mutation_vec = np.random.choice(2, len(individual), p=[1.0-self.mutation_rate, self.mutation_rate])
+			combined_vec = []
+
+			for i,m in enumerate(mutation_vec):
+				if m == 1:
+					#random mutation
+					rand = randint(self.min_value, self.max_value)
+					combined_vec.append(rand)
+				else:
+					#no mutation
+					combined_vec.append(individual[i])
+			mutated_population.append(combined_vec)
+
+		#now we preserve our best individuals and drop the last x mutated individuals
+		num_best = int(self.winner_pool*self.population_size)
+		self.population = self.population[:num_best] + mutated_population[:len(mutated_population)-num_best]
+
+	def breed(self, replace=True):
+		""" given the top x percent breed new solutions """
+		
+		num_breeders = int(self.winner_pool*self.population_size)
+		breeders = self.population[:num_breeders]
+
+		num_children = self.population_size - num_breeders
+		pairings = np.random.choice(num_breeders, num_children, replace=replace)
+		children = []
+
+		for i,pair in enumerate(pairings):
+			i1 = int(i%len(breeders))
+			i2 = int(pair%len(breeders))
+			parent1 = breeders[i1]
+			parent2 = breeders[i2]
+			child_vector = []
+			#create our vector [0,1,0,0..] which chooses which item to take from each individual
+			breed_vector = np.random.choice(2, len(breeders[0])) 
+			for i,v in enumerate(breed_vector):
+				if v == 0:
+					child_vector.append(parent1[i])
+				else:
+					child_vector.append(parent2[i])
+			children.append(child_vector)
+		#now create our new population
+		self.population = self.population[:num_breeders] + children
+
+	def evaluate(self):
+		""" 
+		evaluate the fitness of each individual
+		sort the individuals by fitness (descending order with most fit first)
+		if any individual is of max_fitness then return true, else false
+		"""
+		scored = []
+		for individual in self.population:
+			fitness = self.fitness_func(individual)
+			scored.append((individual,fitness))
+		#sort our individuals by fitness (Descending)
+		sorted_pop = sorted(scored, reverse=True, key=lambda x: x[1])
+
+		#sort our population in descending fitness
+		self.population = [x[0] for x in sorted_pop]
+		#return just our fitness scores
+		return [x[1] for x in sorted_pop]
+
+	def evolve(self):
+		""" main for-loop for genetic algorithm"""
+		for i in range(0,self.max_generations):
+			pop_fitness = self.evaluate()
+			if self.verbose == True:
+				print("Generation: ", i, " Top fitness: ", pop_fitness[0])
+			if pop_fitness[0] >= self.max_fitness:
+				return self.population
+			self.breed()
+			self.mutate()
+		#reached max generations without getting a max_fitness
+		return self.population
+
---
+++ examples/traveling_salesman.py
@@ -0,0 +1,128 @@
+"""
+
+Attempts to solve the traveling salesman problem using genetic algorithms
+
+"""
+
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+from base import Transposon
+import geopy.distance
+
+cities = {
+	"Fargo, ND": [46.877186, -96.789803],
+	"Odessa, TX": [31.845682,-102.367645],
+	"Orangeburg, SC": [33.493317, -80.855415],
+	"McAllen, TX": [26.203407,-98.230011],
+	"Springfield, OH": [39.925751,-83.806694],
+	"Pasadena, TX": [29.691063,-95.209099],
+	"Lake Worth, FL":[26.616756,-80.068451],
+	"Novi, MI": [42.480591,-83.475494],
+	"Rochester, MN": [44.016369,-92.475395],
+	"London, KY": [37.129986,-84.084122],
+	"Santa Clarita, CA": [34.391663,-118.542587],
+	"Overland Park, KS": [38.984764,-94.677658],
+	"St Cloud, MN": [45.560230,-94.172852],
+	"Santee, CA": [32.838383,-116.973915],
+	"Bay Point, CA": [38.033878,-121.960709],
+	"Longmont, CO": [40.167206,-105.101929],
+	"Issaquah, WA": [47.530102,-122.032616],
+	"Akron, OH": [41.081757,-81.511452],
+	"Hammond, IN": [41.584660,-87.500160],
+	"Youngstown, OH": [41.102970,-80.647247],
+	"Hugo, MN": [45.159966,-92.993340],
+	"Yorba Linda, CA": [33.888504,-117.813255],
+	"Smiths Station, AL": [32.540138,-85.098549],
+	"Rapid City, SD": [44.080544,-103.231018],
+	"Hillsboro, OR": [45.522896,-122.989830],
+	"Elkhart, IN": [41.681992,-85.976669],
+	"Providence, RI": [41.825226,-71.418884],
+	"Rancho Cucamonga, CA": [34.110489,-117.594429],
+	"Orange, CA": [33.787914,-117.853104],
+	"Madison, WI": [43.073051,-89.401230],
+	"Jersey City, NJ": [40.728157,-74.077644],
+	"Bradenton, FL": [27.498928,-82.574821],
+	"South Burlington, VT": [44.466995,-73.170959],
+	"Long Beach, CA": [33.770050,-118.193741],
+	"Greenville, NC": [35.612659,-77.366356],
+	"Edison, NJ": [40.522964,-74.411674],
+	"Yakima, WA": [46.602070,-120.505898],
+	"Dunedin, FL": [28.018349,-82.764473],
+	"St Paul, MN": [44.949642,-93.093124],
+	"Kent, WA": [47.380932,-122.234840]
+}
+
+MAX_FITNESS = 9999999
+CITYLIST = []
+
+def main():
+	#turn each city into an ordered list
+	for i,city in enumerate(cities):
+		CITYLIST.append(city)
+
+	max_value = len(cities)-1
+	#instantiate our transposon class
+	transposon = Transposon(
+		vector_len=len(cities),
+		fitness_func=fitness_func,
+		min_value=0,
+		max_value=max_value,
+		population_size=1000,
+		max_fitness=MAX_FITNESS,
+		max_generations=1000,
+		mutation_rate=0.13,
+		verbose=True)
+	results = transposon.evolve()
+	#given the best result, print the path
+	print("Optimal path")
+	best_vector = convert_vector(results[0])
+	for city in best_vector:
+		print(city)
+	#now print the total distance
+	print("Total Distance:", MAX_FITNESS - fitness_func(results[0]))
+
+
+def convert_vector(vector):
+	""" converts a vector of integers (which can have duplicates)
+	to a single list of cities (no duplicates)
+	replaces duplicates in a repeatable manner (same vector will yield the same result each time)
+	"""
+	city_list = CITYLIST.copy()
+	city_vector = []
+	for idx in vector:
+		abs_idx = idx % len(city_list)
+		city = city_list[abs_idx]
+		city_vector.append(city)
+		city_list.pop(abs_idx)
+	return city_vector
+
+def fitness_func(vector):
+	""" 
+	in this simulation our max_fitness is unknown
+	so we create a ceiling amount and will subtract the total distance from this amount
+	fitness = MAX_FITNESS - distance
+	This means we will never quit until the simulation has run through maxium amount of trials
+	however, we don't know the minimum distance ahead of time so we cannot verify we have hit the global optimum
+	"""
+	total_distance = 0
+	city_vector = convert_vector(vector)
+	for i,city in enumerate(city_vector):
+		if i == len(city_vector) - 1:
+			#final case, calculate distance from last city to first
+			city1 = cities[city]
+			city2 = cities[city_vector[0]]
+			total_distance += calculate_distance(city1, city2)
+		else:
+			city1 = cities[city]
+			city2 = cities[city_vector[i+1]]
+			total_distance += calculate_distance(city1, city2)
+	return MAX_FITNESS - total_distance
+
+def calculate_distance(coords_1, coords_2):
+	""" given two coordinates (lattitude and longitude)
+	calculates the distance in kilometers """
+	return geopy.distance.vincenty(coords_1, coords_2).km
+
+
+if __name__ == "__main__":
+	main()
